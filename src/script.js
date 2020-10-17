202bbf9520a115dbd362f9d17ddc4159457f9924
Rahulruben <mulerahulruben@gmail.com>
2020-10-17 14:21:23 +0530

    Toggled states on clicking

--- src/script.js
+++ src/script.js
@@ -2,13 +2,21 @@ const tabExample = document.getElementById('tab-example');
 const panels = tabExample.querySelectorAll('[role="tabpanel"]');
 const tabBtns = tabExample.querySelectorAll('[role="tab"]');
 
+const getActiveTabButton = () => {
+  return [...tabBtns].find(tab => tab.getAttribute('aria-selected'));
+}
+
+const setActiveTabButton = (tabButton, state) => {
+  tabButton.setAttribute('aria-selected', state);
+}
+
 const setInitialTabState = () => {
   tabBtns[0].setAttribute('aria-selected', true);
   panels[0].setAttribute('active', '');
 }
 
 const addListeners = () => {
-  tabBtns.forEach(tab => tab.addEventListener('click', _ => {
+  tabBtns.forEach(tab => tab.addEventListener('click', e => {
     const panelId = tab.getAttribute('aria-controls');
     [...panels].map(panel => {
       if (panel.id === panelId) {
@@ -17,8 +25,11 @@ const addListeners = () => {
         panel.removeAttribute('active');
       }
     })
-    
+    console.log(getActiveTabButton())
+    setActiveTabButton(getActiveTabButton(), false);
+    setActiveTabButton(e.target, true);
   }))
+  
 }
 
 setInitialTabState();
